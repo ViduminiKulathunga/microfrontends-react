17f9d3682839c15c4b44106b5e4a9374b3dc83c5
ViduminiKulathunga <viduminikulathunga@gmail.com>
2025-04-27 18:34:45 +0200

    Production domain env varible fixed in main.yml

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 const App = () => {
   return (
     <div>
-      <h1>Hi Vidu!</h1>
+      <h1>Hi Vidu!!!</h1>
       <hr />
       <MarketingApp />
     </div>
