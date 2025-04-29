446108a530367f79fd0bc5f9a59745daf0a5c453
ViduminiKulathunga <viduminikulathunga@gmail.com>
2025-04-29 20:33:50 +0200

    Auth micro frontend

---
+++ packages/auth/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Switch, Route, Router } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+import SignIn from "./components/Signin";
+import SignUp from "./components/Signup";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "au",
+});
+
+export default ({ history }) => {
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <Router history={history}>
+          <Switch>
+            <Route exact path="/auth/signup" component={SignUp} />
+            <Route exact path="/auth/signin" component={SignIn} />
+          </Switch>
+        </Router>
+      </StylesProvider>
+    </div>
+  );
+};
