7976a8347ce627f60c75473e5752edba0ea4f432
ViduminiKulathunga <viduminikulathunga@gmail.com>
2025-04-29 22:12:37 +0200

    Auth Deploy Config .yml

--- packages/auth/src/boostrap.js
+++ packages/auth/src/boostrap.js
@@ -3,7 +3,7 @@ import ReactDom from "react-dom";
 import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
-const mount = (el, { onNaviagte, defaultHistory, initialPath }) => {
+const mount = (el, { onSignIn, onNaviagte, defaultHistory, initialPath }) => {
   const history =
     defaultHistory ||
     createMemoryHistory({
@@ -14,7 +14,7 @@ const mount = (el, { onNaviagte, defaultHistory, initialPath }) => {
     history.listen(onNaviagte);
   }
 
-  ReactDom.render(<App history={history} />, el);
+  ReactDom.render(<App onSignIn={onSignIn} history={history} />, el);
 
   return {
     onParentNavigate({ pathname: nextPathname }) {
